214acbf4ad35edb31013eb465e18b33bd0cc3cd3
eboc-2020 <70177453+eboc-2020@users.noreply.github.com>
2022-07-27 16:17:45 -0600

    Creacion de una version 2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\v1\PostController as PostV1;
+use App\Http\Controllers\Api\v2\PostController as PostV2;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::apiResource('v1/posts',App\Http\Controllers\Api\v1\PostController::class)
+//V1
+Route::apiResource('v1/posts',PostV1::class)
+->only(['index','show','destroy']);
+//V2
+Route::apiResource('v2/posts',PostV2::class)
 ->only(['index','show','destroy']);
