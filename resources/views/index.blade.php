62a79b828af70c468456ff00e457d2c616e33435
eboc-2020 <70177453+eboc-2020@users.noreply.github.com>
2022-07-27 09:20:08 -0600

    Instalacion de taildwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
+    <p><strong>Hell world!</strong></p>
+    <div class="w-64 h-64 bg-red-200"></div>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     @foreach ($posts as $post)
         <div>
             <h2>{{$post->title}}</h2>
